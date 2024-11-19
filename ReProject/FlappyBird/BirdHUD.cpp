06a1b81f2cc70f0cffd65e00fe1481eb110eaf3a
RookStein <zhutong1993@163.com>
2024-11-19 10:01:15 +0800

    初始画面

--- ReProject/FlappyBird/BirdHUD.cpp
+++ ReProject/FlappyBird/BirdHUD.cpp
@@ -1,2 +1,62 @@
 #include "BirdHUD.h"
+#include "Engine/Canvas.h"
+ABirdHUD::ABirdHUD()
+{
+	GameplayButtonTexturePath = TEXT("/Script/Engine.Texture2D'/Game/_Game/Texture/UI/button_play.button_play'");
+	bDrawGamePlayButton = true;
+	GamePlayButtonTexture = nullptr;
+}
+
+void ABirdHUD::BeginPlay()
+{
+	Super::BeginPlay();
+	//初始化按钮
+	BeginPlayButtonInit();
+}
+void ABirdHUD::BeginPlayButtonInit()
+{
+	GamePlayButtonTexture = LoadObject<UTexture>(nullptr,*GameplayButtonTexturePath);
+	Texture2D = Cast<UTexture2D>(GamePlayButtonTexture);
+	TextureWidth = Texture2D->GetSizeX();
+	TextureHeight = Texture2D->GetSizeY();
+	UE_LOG(LogTemp, Warning, TEXT("Width: %.2f, Height: %.2f"), TextureWidth, TextureHeight);
+
+}
+void ABirdHUD::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABirdHUD::DrawHUD()
+{
+	Super::DrawHUD();
+	ScreenWidth = Canvas->SizeX/2 -TextureWidth + 60;
+	ScreenHeight = Canvas->SizeY/2 - TextureHeight;
+	if (bDrawGamePlayButton)
+	{
+		DrawGamePlayButton();
+	}
+}
+void ABirdHUD::DrawGamePlayButton()
+{
+	if (GamePlayButtonTexture != nullptr)
+	{
+		DrawTextureSimple(GamePlayButtonTexture,ScreenWidth,ScreenHeight);
+	}else
+	{
+		GamePlayButtonTexture = LoadObject<UTexture>(nullptr,*GameplayButtonTexturePath);
+	}
+}
+
+void ABirdHUD::NotifyHitBoxClick(FName BoxName)
+{
+	Super::NotifyHitBoxClick(BoxName);
+}
+
+void ABirdHUD::NotifyHitBoxRelease(FName BoxName)
+{
+	Super::NotifyHitBoxRelease(BoxName);
+}
+
+
 
--- ReProject/FlappyBird/BirdHUD.h
+++ ReProject/FlappyBird/BirdHUD.h
@@ -9,5 +9,24 @@ UCLASS()
 class REPROJECT_API ABirdHUD : public AHUD
 {
 	GENERATED_BODY()
+public:
+	ABirdHUD();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void DrawHUD() override;
+	virtual void NotifyHitBoxClick(FName BoxName) override;
+	virtual void NotifyHitBoxRelease(FName BoxName) override;
+	void DrawGamePlayButton();
+	bool bDrawGamePlayButton;
+	UTexture* GamePlayButtonTexture;
+	void BeginPlayButtonInit();
+	
+
+private:
+	FString GameplayButtonTexturePath;
+	float TextureWidth,TextureHeight;
+	float ScreenWidth;
+	float ScreenHeight;
+	UTexture2D* Texture2D;
 	
 };
